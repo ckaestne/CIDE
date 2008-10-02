8f16521f1bade4c074f9ce898b0dd07ba1a02d54
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2008-10-02 15:11:20 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@474 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_Language_CS/src/de/ovgu/cide/language/cs/CSLanguageExtension.java
+++ CIDE_Language_CS/src/de/ovgu/cide/language/cs/CSLanguageExtension.java
@@ -3,13 +3,11 @@ package de.ovgu.cide.language.cs;
 import java.io.InputStream;
 
 import tmp.generated_cs.CSParser;
-import tmp.generated_cs.SimplePrintVisitor;
 import cide.gast.ISourceFile;
 import cide.gparser.OffsetCharStream;
 import cide.gparser.ParseException;
 import cide.languages.ILanguageExtension;
 import cide.languages.ILanguageParser;
-import cide.languages.ILanguagePrintVisitor;
 import cide.languages.ILanguageValidator;
 
 public class CSLanguageExtension implements ILanguageExtension {
@@ -21,10 +19,6 @@ public class CSLanguageExtension implements ILanguageExtension {
 			public ISourceFile getRoot() throws ParseException {
 				return new CSParser(new OffsetCharStream(inputStream)).compilation_unit();
 			}};
-	}
-
-	public ILanguagePrintVisitor getPrettyPrinter() {
-		return new SimplePrintVisitor();
 	}
 
 	public ILanguageValidator getValidator() {
