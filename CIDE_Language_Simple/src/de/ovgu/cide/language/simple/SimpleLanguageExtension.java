fcf084356aed56623e4b9610fe7ce6f59370cd74
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2008-11-04 14:10:40 +0000

    updated languages for new framework
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@492 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_Language_Simple/src/de/ovgu/cide/language/simple/SimpleLanguageExtension.java
+++ CIDE_Language_Simple/src/de/ovgu/cide/language/simple/SimpleLanguageExtension.java
@@ -3,14 +3,11 @@ package de.ovgu.cide.language.simple;
 import java.io.InputStream;
 
 import tmp.generated_simple.JavaParser;
-import tmp.generated_simple.SimplePrintVisitor;
 import cide.gast.ISourceFile;
 import cide.gparser.OffsetCharStream;
 import cide.gparser.ParseException;
 import cide.languages.ILanguageExtension;
 import cide.languages.ILanguageParser;
-import cide.languages.ILanguagePrintVisitor;
-import cide.languages.ILanguageValidator;
 
 public class SimpleLanguageExtension implements ILanguageExtension {
 
@@ -29,13 +26,13 @@ public class SimpleLanguageExtension implements ILanguageExtension {
 		};
 	}
 
-	public ILanguagePrintVisitor getPrettyPrinter() {
-		return new SimplePrintVisitor();
-	}
-
-	public ILanguageValidator getValidator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+//	public ILanguagePrintVisitor getPrettyPrinter() {
+//		return new SimplePrintVisitor();
+//	}
+//
+//	public ILanguageValidator getValidator() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
 
 }
