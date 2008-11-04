60e0e5a5f69d62adcf2b753601d9ba2ae992c10a
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2008-11-04 14:10:21 +0000

    updated languages for new framework
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@490 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_Language_Bali/src/de/ovgu/cide/language/bali/BaliLanguageExtension.java
+++ CIDE_Language_Bali/src/de/ovgu/cide/language/bali/BaliLanguageExtension.java
@@ -3,14 +3,11 @@ package de.ovgu.cide.language.bali;
 import java.io.InputStream;
 
 import tmp.generated_bali.BaliParser;
-import tmp.generated_bali.SimplePrintVisitor;
 import cide.gast.ISourceFile;
 import cide.gparser.OffsetCharStream;
 import cide.gparser.ParseException;
 import cide.languages.ILanguageExtension;
 import cide.languages.ILanguageParser;
-import cide.languages.ILanguagePrintVisitor;
-import cide.languages.ILanguageValidator;
 
 public class BaliLanguageExtension implements ILanguageExtension {
 
@@ -28,14 +25,14 @@ public class BaliLanguageExtension implements ILanguageExtension {
 		};
 
 	}
+//
+//	public ILanguagePrintVisitor getPrettyPrinter() {
+//		return new SimplePrintVisitor();
+//	}
 
-	public ILanguagePrintVisitor getPrettyPrinter() {
-		return new SimplePrintVisitor();
-	}
-
-	public ILanguageValidator getValidator() {
-//		return new BaliValidator();
-		return null;
-	}
+//	public ILanguageValidator getValidator() {
+////		return new BaliValidator();
+//		return null;
+//	}
 
 }
