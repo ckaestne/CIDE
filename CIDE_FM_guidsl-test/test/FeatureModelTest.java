2eff068c66fc3d80af108e69c1f4f5fbc224a0e6
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2008-11-19 19:34:23 +0000

    major rewrite to implement typechecking framework
    CLOSED - # 36: Add Validation Support (language-independent type-checking framework) 
    https://faracvs.cs.uni-magdeburg.de/projects/ckaestne-coloredIDE/ticket/36
    
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@598 16d93a9b-e91d-0410-b077-e6ebf86c48e5

---
+++ CIDE_FM_guidsl-test/test/FeatureModelTest.java
@@ -0,0 +1,164 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import junit.framework.Assert;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.junit.Before;
+import org.junit.Test;
+import org.sat4j.specs.TimeoutException;
+
+import featureide.fm.core.model.Feature;
+import featureide.fm.core.model.FeatureModel;
+import featureide.fm.core.model.io.UnsupportedModelException;
+import featureide.fm.core.model.io.guidsl.FeatureModelReader;
+
+public class FeatureModelTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	private IFile createModelFile(String content) throws CoreException {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"test_generated");
+		if (!project.exists())
+			project.create(null);
+		if (!project.isOpen())
+			project.open(null);
+		IFile file = project.getFile("_testfile");
+		if (file.exists())
+			file.delete(true, true, null);
+		file.create(new ByteArrayInputStream(content.getBytes()), true, null);
+		return file;
+	}
+
+	private FeatureModel createFeatureModel(String content)
+			throws CoreException, UnsupportedModelException, IOException {
+		IFile file = createModelFile(content);
+		FeatureModel model = new FeatureModel();
+		FeatureModelReader modelReader = new FeatureModelReader(model);
+		modelReader.loadFromFile(file);
+		return model;
+	}
+
+	@Test
+	public void testFeatureModelCreation() throws CoreException,
+			UnsupportedModelException, IOException {
+		FeatureModel model = createFeatureModel("A: B C::_A;");
+		System.out.println(model.getFeatureNames());
+		Assert.assertNotNull(model);
+	}
+
+	@Test
+	public void testIsValid() throws CoreException, UnsupportedModelException,
+			IOException, TimeoutException {
+
+		Assert.assertTrue(createFeatureModel(
+				"A: [B] [C] ::_A;\n%%\nB implies C;").isValid());
+		Assert.assertFalse(createFeatureModel(
+				"A: [B] [C] ::_A;\n%%\nB and C;B implies not C;").isValid());
+
+	}
+
+	@Test
+	public void testImplicationSimple() throws CoreException,
+			UnsupportedModelException, IOException, TimeoutException {
+
+		FeatureModel model = createFeatureModel("A: [B] [C] [D] ::_A;\n%%\nB implies C;");
+		Feature A = model.getFeature("A");
+		Feature B = model.getFeature("B");
+		Feature C = model.getFeature("C");
+		Feature D = model.getFeature("D");
+
+		assertImpl(model, Collections.singleton(A), Collections.singleton(A));
+		assertNotImpl(model, Collections.singleton(A), Collections.singleton(B));
+		assertImpl(model, Collections.singleton(B), Collections.singleton(A));
+		assertImpl(model, L(A, B), Collections.singleton(B));
+		assertImpl(model, Collections.singleton(B), Collections.singleton(C));
+		assertImpl(model, Collections.singleton(B), Collections.EMPTY_SET);
+		assertNotImpl(model, Collections.EMPTY_SET, Collections.singleton(C));
+		assertImpl(model, L(B, D), L(C, D));
+		assertImpl(model, L(A, B), Collections.singleton(C));
+
+	}
+
+	@Test
+	public void testInvalidImplication() throws CoreException,
+			UnsupportedModelException, IOException, TimeoutException {
+
+		FeatureModel model = createFeatureModel("A: [B] [C] ::_A;\n%%\nB and C;B implies not C;");
+		Feature A = model.getFeature("A");
+		Feature B = model.getFeature("B");
+
+		assertImpl(model, Collections.singleton(B), Collections.singleton(A));
+	}
+
+	@Test
+	public void testImplicationDeadFeatures() throws CoreException,
+			UnsupportedModelException, IOException, TimeoutException {
+
+		FeatureModel model = createFeatureModel("A: [B] [C] [D] ::_A;\n%%\nA implies not C;D implies C;");
+		Feature A = model.getFeature("A");
+		Feature B = model.getFeature("B");
+		Feature C = model.getFeature("C");
+		Feature D = model.getFeature("D");
+
+		assertImpl(model, Collections.singleton(C), Collections.singleton(D));
+		assertImpl(model, Collections.singleton(D), Collections.singleton(C));
+		assertNotImpl(model, Collections.singleton(A), Collections.singleton(C));
+		assertNotImpl(model, Collections.singleton(B), Collections.singleton(C));
+		assertImpl(model, Collections.singleton(C), Collections.singleton(B));
+		assertImpl(model, L(B, C), Collections.singleton(A));
+
+	}
+
+	@Test
+	public void testImplicationSubset() throws CoreException,
+			UnsupportedModelException, IOException, TimeoutException {
+
+		FeatureModel model = createFeatureModel("A: [B] [C] [D] ::_A;");
+		Feature A = model.getFeature("A");
+		Feature B = model.getFeature("B");
+		Feature C = model.getFeature("C");
+		Feature D = model.getFeature("D");
+
+		assertImpl(model, L(L(B, C), D), L(B, C));
+		assertImpl(model, L(L(B, C), D), L(C, D));
+		assertNotImpl(model, L(C, D), L(L(B, C), D));
+		assertImpl(model, L(L(B, C), A), L(B, C));
+		assertImpl(model, L(L(B, C), D), Collections.singleton(C));
+
+	}
+
+	private Set<Feature> L(Feature a, Feature b) {
+		HashSet<Feature> result = new HashSet<Feature>();
+		result.add(a);
+		result.add(b);
+		return result;
+	}
+
+	private Set<Feature> L(Set<Feature> set, Feature b) {
+		set.add(b);
+		return set;
+	}
+
+	private void assertImpl(FeatureModel model, Set<Feature> a, Set<Feature> b)
+			throws TimeoutException {
+		Assert.assertTrue("assume: " + a + "=>" + b, model.checkImplies(a, b));
+	}
+
+	private void assertNotImpl(FeatureModel model, Set<Feature> a,
+			Set<Feature> b) throws TimeoutException {
+		Assert.assertFalse("assume not: " + a + "=>" + b, model.checkImplies(a,
+				b));
+	}
+
+}
