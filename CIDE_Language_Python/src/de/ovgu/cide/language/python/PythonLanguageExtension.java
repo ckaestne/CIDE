34d502a77f1dc11ad56aca018f8670006c50e6de
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2009-01-26 19:00:56 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@657 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_Language_Python/src/de/ovgu/cide/language/python/PythonLanguageExtension.java
+++ CIDE_Language_Python/src/de/ovgu/cide/language/python/PythonLanguageExtension.java
@@ -2,25 +2,31 @@ package de.ovgu.cide.language.python;
 
 import java.io.InputStream;
 
+import tmp.generated_python.PythonParser;
+import cide.gast.ISourceFile;
+import cide.gparser.OffsetCharStream;
+import cide.gparser.ParseException;
 import cide.languages.ILanguageExtension;
 import cide.languages.ILanguageParser;
 
 public class PythonLanguageExtension implements ILanguageExtension {
 
 	public PythonLanguageExtension() {
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	public ILanguageParser getParser(final InputStream inputStream,
+			String filename) {
+		return new ILanguageParser() {
+			public ISourceFile getRoot() throws ParseException {
+				return new PythonParser(new OffsetCharStream(inputStream)).file_input();
+			}
+		};
+	}
+
+	public static final String LANGUAGE_EXTENSION_ID = "de.ovgu.cide.language.python";
+
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ILanguageParser getParser(InputStream inputStream, String filePath) {
-		// TODO Auto-generated method stub
-		return null;
+		return LANGUAGE_EXTENSION_ID;
 	}
 
 }
