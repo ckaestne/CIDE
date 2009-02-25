25a3fe8cded94dd5e525d55ea6aa09225d0532c1
ex_rosenthal <ex_rosenthal@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2009-02-25 11:03:59 +0000

    Testfälle erweitert, v.a. für komplettes CFJ-Typsystem (ohne alternative Features)
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@699 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CaseStudies/FJTrials/model.m
+++ CaseStudies/FJTrials/model.m
@@ -1,10 +1,13 @@
 Project_ : Project+ :: _Project ;
 
-Project : Feature1
-	| Feature2
+Project : Feature1 Feature2 :: Feature1ANDFeature2
 	| Feature3
 	| Feature4
 	| Feature5
 	| Feature6
-	| Feature7 ;
+	| Feature7
+	| Feature8XORFeature9 ;
+
+Feature8XORFeature9 : Feature8
+	| Feature9 ;
 
