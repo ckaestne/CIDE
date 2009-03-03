b4bbb00ded79a665a5c6ff7cb06ca485a3117420
ex_rosenthal <ex_rosenthal@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2009-03-03 15:35:31 +0000

    EvaluationStrategy aufgebohrt
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@702 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_FM_guidsl/src/de/ovgu/cide/typing/guidsl/SatEvaluationStrategy.java
+++ CIDE_FM_guidsl/src/de/ovgu/cide/typing/guidsl/SatEvaluationStrategy.java
@@ -1,6 +1,8 @@
 package de.ovgu.cide.typing.guidsl;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import org.sat4j.specs.TimeoutException;
@@ -27,37 +29,83 @@ import featureide.fm.core.FeatureModel;
 public class SatEvaluationStrategy extends AbstractCachingEvaluationStrategy {
 
 	@Override
-	protected boolean calcImplies(IFeatureModel featureModel,
-			Set<IFeature> source, Set<IFeature> target) {
+	protected boolean calcImplies(IFeatureModel featureModel, Set<IFeature> source, Set<IFeature> target) {
 		// ignore empty feature models
 		if (featureModel instanceof EmptyFeatureModel)
 			return true;
 		assert featureModel instanceof GuidslFeatureModelWrapper;
 
-		FeatureModel guidslModel = ((GuidslFeatureModelWrapper) featureModel)
-				.getInternalModel();
+		FeatureModel guidslModel = ((GuidslFeatureModelWrapper) featureModel).getInternalModel();
 
-		Set<Feature> guidslSourceFeatures = new HashSet<Feature>(source.size());
-		for (IFeature sourceFeature : source) {
-			assert sourceFeature instanceof FeatureAdapter;
-			guidslSourceFeatures.add(((FeatureAdapter) sourceFeature)
-					.getInternalFeature());
-		}
-		Set<Feature> guidslTargetFeatures = new HashSet<Feature>(source.size());
-		for (IFeature targetFeature : target) {
-			assert targetFeature instanceof FeatureAdapter;
-			guidslTargetFeatures.add(((FeatureAdapter) targetFeature)
-					.getInternalFeature());
-		}
+		Set<Feature> guidslSourceFeatures = convertToGuidslFeatures(source);
+		Set<Feature> guidslTargetFeatures = convertToGuidslFeatures(target);
 
 		try {
-			return guidslModel.checkImplies(guidslSourceFeatures,
-					guidslTargetFeatures);
+			return guidslModel.checkImplies(guidslSourceFeatures, guidslTargetFeatures);
 		} catch (TimeoutException e) {
 			e.printStackTrace();
 			// in case of a timeout assume everything is fine and the
 			// implication is true. idea is not to report false positive
 			return true;
 		}
+	}
+
+	@Override
+	public boolean areMutualExclusive(IFeatureModel featureModel, Set<IFeature> context, List<Set<IFeature>> featureSets) {
+		// ignore empty feature models
+		if (featureModel instanceof EmptyFeatureModel)
+			return true;
+		assert featureModel instanceof GuidslFeatureModelWrapper;
+		
+		List<Set<Feature>> guidslFeatureSets = new LinkedList<Set<Feature>>();
+		for (Set<IFeature> features : featureSets) {
+			guidslFeatureSets.add(convertToGuidslFeatures(features));
+		}
+
+		FeatureModel guidslModel = ((GuidslFeatureModelWrapper) featureModel).getInternalModel();
+		try {
+			return guidslModel.areMutualExclusive(convertToGuidslFeatures(context), guidslFeatureSets);
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+			// in case of a timeout assume everything is fine and the
+			// implication is true. idea is not to report false positive
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean mayBeMissing(IFeatureModel featureModel, Set<IFeature> context, List<Set<IFeature>> featureSets) {
+		// ignore empty feature models
+		if (featureModel instanceof EmptyFeatureModel)
+			return true;
+		assert featureModel instanceof GuidslFeatureModelWrapper;
+		
+		List<Set<Feature>> guidslFeatureSets = new LinkedList<Set<Feature>>();
+		for (Set<IFeature> features : featureSets) {
+			guidslFeatureSets.add(convertToGuidslFeatures(features));
+		}
+
+		FeatureModel guidslModel = ((GuidslFeatureModelWrapper) featureModel).getInternalModel();
+		try {
+			return guidslModel.mayBeMissing(convertToGuidslFeatures(context), guidslFeatureSets);
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+			// in case of a timeout assume everything is fine and the
+			// implication is true. idea is not to report false positive
+			return true;
+		}
+	}
+	
+	private Set<Feature> convertToGuidslFeatures(Set<IFeature> features) {
+		if (features == null)
+			return null;
+		
+		Set<Feature> guidslFeatures = new HashSet<Feature>(features.size());
+		for (IFeature f : features) {
+			assert f instanceof FeatureAdapter;
+			guidslFeatures.add(((FeatureAdapter) f).getInternalFeature());
+		}
+		
+		return guidslFeatures;
 	}
 }
