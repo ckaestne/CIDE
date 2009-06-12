18650d84fbdd35450c8640a5b11278c4fcedbe05
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2009-06-12 16:19:08 +0000

    CLOSED - # 61: Type checking should be started automatically https://faracvs.cs.uni-magdeburg.de/projects/ckaestne-coloredIDE/ticket/61
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@772 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE2_TypeChecking/src/de/ovgu/cide/typing/CIDETypingPlugin.java
+++ CIDE2_TypeChecking/src/de/ovgu/cide/typing/CIDETypingPlugin.java
@@ -1,5 +1,6 @@
 package de.ovgu.cide.typing;
 
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
 
@@ -17,7 +18,7 @@ public class CIDETypingPlugin extends Plugin {
 	private static CIDETypingPlugin plugin;
 
 	private TypingManager typingManager;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -26,18 +27,27 @@ public class CIDETypingPlugin extends Plugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		System.out.println("starting CIDE type system");
 		plugin = this;
-		typingManager=new TypingManager();
+		typingManager = new TypingManager();
 		typingManager.register();
+
+		// initial type-check
+		CIDETypingPlugin.getDefault().getTypingManager().recheckProjects(
+				ResourcesPlugin.getWorkspace().getRoot().getProjects());
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -47,7 +57,7 @@ public class CIDETypingPlugin extends Plugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static CIDETypingPlugin getDefault() {
