7a3de70d726c694262b80fd3563b7376601a7151
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2010-03-26 15:45:13 +0000

    new license
    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@992 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE_generateAST/src/org/javacc/parser/RCharacterList.java
+++ CIDE_generateAST/src/org/javacc/parser/RCharacterList.java
@@ -1,3 +1,23 @@
+/**
+    Copyright 2010 Christian Kästner
+
+    This file is part of CIDE.
+
+    CIDE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    CIDE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CIDE.  If not, see <http://www.gnu.org/licenses/>.
+
+    See http://www.fosd.de/cide/ for further information.
+*/
+
 /*
  * Copyright Â© 2002 Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
  * California 95054, U.S.A. All rights reserved.  Sun Microsystems, Inc. has
