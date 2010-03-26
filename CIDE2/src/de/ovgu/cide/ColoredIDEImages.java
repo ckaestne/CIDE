8e88e9d149663c19e0d3d4c741d6cfd01ce87748
ckaestne <ckaestne@16d93a9b-e91d-0410-b077-e6ebf86c48e5>
2010-03-26 15:42:40 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/ckaestne-coloredIDE/trunk@990 16d93a9b-e91d-0410-b077-e6ebf86c48e5

--- CIDE2/src/de/ovgu/cide/ColoredIDEImages.java
+++ CIDE2/src/de/ovgu/cide/ColoredIDEImages.java
@@ -1,13 +1,22 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+/**
+    Copyright 2010 Christian Kästner
+
+    This file is part of CIDE.
+
+    CIDE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    CIDE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CIDE.  If not, see <http://www.gnu.org/licenses/>.
+
+    See http://www.fosd.de/cide/ for further information.
+*/
 package de.ovgu.cide;
 
 import java.net.URL;
--- CIDE2_TypeChecking/src/de/ovgu/cide/typing/model/ITypingMarkerResolution.java
+++ CIDE2_TypeChecking/src/de/ovgu/cide/typing/model/ITypingMarkerResolution.java
@@ -1,3 +1,23 @@
+/**
+    Copyright 2010 Christian Kästner
+
+    This file is part of CIDE.
+
+    CIDE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    CIDE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CIDE.  If not, see <http://www.gnu.org/licenses/>.
+
+    See http://www.fosd.de/cide/ for further information.
+*/
+
 package de.ovgu.cide.typing.model;
 
 import org.eclipse.ui.IMarkerResolution;
